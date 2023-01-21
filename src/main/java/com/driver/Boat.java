76940f9a7aa70397ad7cac6849fdb64cea62fdb7
Arvind Kumar <arvindkr40882@gmail.com>
2023-01-21 15:01:38 +0530

    remove abstract class

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -1,12 +1,23 @@
 package com.driver;
 
-public abstract class Boat implements WaterVehicle{
+public class Boat implements WaterVehicle{
     private final String name;
-    private int capacity;
+    private final int capacity;
 public Boat(String name,int capacity){
     this.name=name;
     this.capacity=capacity;
 }
+
+    @Override
+    public String getVehicleName() {
+        return null;
+    }
+
+    @Override
+    public int getVehicleCapacity() {
+        return 0;
+    }
+
     @Override
     public String getVehicalName() {
         return name;
