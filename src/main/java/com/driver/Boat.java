41e432da4a1a736f71132b3a7e02e7c65413bfa3
Arvind Kumar <arvindkr40882@gmail.com>
2023-01-21 14:45:53 +0530

    make it abstract class

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -1,29 +1,19 @@
 package com.driver;
 
-public class Boat implements WaterVehicle{
+public abstract class Boat implements WaterVehicle{
     private final String name;
-    private final int capacity;
-    public Boat(String name,int capacity){
-        this.name=name;
-        this.capacity=capacity;
+    private int capacity;
+public Boat(String name,int capacity){
+    this.name=name;
+    this.capacity=capacity;
+}
+    @Override
+    public String getVehicalName() {
+        return name;
     }
 
     @Override
-    public String getVehicleName() {
-        return null;
-    }
-
-    @Override
-    public int getVehicleCapacity() {
-        return 0;
-    }
-
-    @Override
-    public String getVehicalName(){
-      return name;
-    }
-    @Override
-    public int getVehicalCapacity(){
+    public int getVehicalCapacity() {
         return capacity;
     }
 }
